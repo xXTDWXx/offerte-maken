83c07f50dd705309258f7faf790740d31dc8a3e3
xXTDWXx <kennysunspabrugge@gmail.com>
2026-04-03 13:37:18 +0200

    Update product.js

--- product.js
+++ product.js
@@ -1241,10 +1241,10 @@ function printOfferte() {
             <div class="info-grid">
               <div class="card">
                 <div class="card-title">Klantgegevens</div>
-                <div class="card-line"><strong>Naam:_____________________</strong> ${customerNameHtml}</div>
-                <div class="card-line"><strong>Adres:_____________________</strong> ${customerStreetHtml}</div>
-                <div class="card-line"><strong>Plaats:_____________________</strong> ${customerCityHtml}</div>
-                <div class="card-line"><strong>Telefoon:_____________________</strong> ${customerPhoneHtml}</div>
+                <div class="card-line"><strong>Naam:</strong> _____________________</div>
+                <div class="card-line"><strong>Adres:</strong> _____________________</div>
+                <div class="card-line"><strong>Plaats:</strong> _____________________</div>
+                <div class="card-line"><strong>Telefoon:</strong> _____________________</div>
               </div>
 
               <div class="card">
